0c1c36fff1fd051705ef22f64a2c92e3906827f2
Judy Chong <judyjchong@gmail.com>
2022-06-03 12:19:36 -0400

    first commit

---
+++ public/main.js
@@ -0,0 +1,74 @@
+const thumbUp = document.getElementsByClassName("fa-thumbs-up");
+const thumbDown = document.getElementsByClassName("fa-thumbs-down");
+const trash = document.getElementsByClassName("fa-trash");
+
+Array.from(thumbUp).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+        // console.log(`this ${thumbUp}`)
+
+        fetch('messages', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg,
+            'thumbUp':thumbUp
+          })
+        })
+        .then(response => {
+          if (response.ok) return response.json()
+        })
+        .then(data => {
+          console.log(data)
+          window.location.reload(true)
+        })
+       });
+});
+
+//This array.from and .for each ensures that each of the thumbDown has a an event listener. The this is a method of addeventlistener like e.target
+Array.from(thumbDown).forEach(function(element) {
+  element.addEventListener('click', function(){
+    const name = this.parentNode.parentNode.childNodes[1].innerText
+    const msg = this.parentNode.parentNode.childNodes[3].innerText
+    const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+    fetch('messagesTDown', {
+      method: 'put',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        'name': name,
+        'msg': msg,
+        'thumbUp':thumbUp
+      })
+    })
+    .then(response => {
+      if (response.ok) return response.json()
+    })
+    .then(data => {
+      console.log(data)
+      window.location.reload(true)
+    })
+  });
+});
+
+Array.from(trash).forEach(function(element) {
+      element.addEventListener('click', function(){
+        //Look at form, it goes from the trashcan <i> to the parent <span> to the parent <li> to the then down into the <span> to get the li for name
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        fetch('messages', {
+          method: 'delete',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg
+          })
+        }).then(function (response) {
+          window.location.reload()
+        })
+      });
+});
